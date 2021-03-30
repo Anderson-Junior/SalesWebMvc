83ecfed9a074a7939168f8bf63ea6bf8c4edd48c
Anderson-Junior <ander.lemos.jr@gmail.com>
2021-03-30 13:46:18 -0300

    Seeding Service

--- SalesWebMvc/Models/Enums/SaleStatus.cs
+++ SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -1,9 +1,9 @@
 ﻿namespace SalesWebMvc.Models.Enums
 {
-    public enum SalesStatus : int
+    public enum SaleStatus : int
     {
         Pending = 0,
-        Bailled = 1,
+        Billed = 1,
         Canceled = 2
     }
 }
